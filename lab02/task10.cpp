f0ca164f28ef3d5d5ae2052603b4815d85571dc4
Richtermnd <m.rustam1305@gmail.com>
2024-11-01 02:07:07 +0500

    Solved skipped task

--- lab02/task10.cpp
+++ lab02/task10.cpp
@@ -1,4 +1,21 @@
-int main() {
+#include <cstdio>
 
+int main() {
+    float x, y, z;
+    scanf("%f %f %f", &x, &y, &z);
+    if (x < y+z && y < x+z && z < x+y)
+        printf("Да\n");
+    else
+        printf("Нет\n");
+
+    bool isRight; // Прямоугольный?
+    if (x*x == y*y + z*z || y*y == x*x + z*z || z*z == x*x + y*y)
+        printf("Прямоугольный\n");
+    else if (x == y && x == z)
+        printf("Равносторонний\n");
+    else if (x == y || x == z || y == z)
+        printf("Равнобедренный\n");
+    else
+        printf("Никакой\n");
     return 0;
 }
